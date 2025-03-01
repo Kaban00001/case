aee53adb8ce9fedd256f834045b5abe225e21c42
Caban <awerinzh@yandex.ru>
2025-03-01 12:14:23 +0300

    1.03

--- src/test/java/my_progect/SelenideRepositorySearchPageObjects.java
+++ src/test/java/my_progect/SelenideRepositorySearchPageObjects.java
@@ -1,40 +1,39 @@
 package my_progect;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.RepositoryPage;
+import pages.components.MainPage;
 import pages.components.WelcomePage;
 
 
 
-public class SelenideRepositorySearchPageObjects {
+public class SelenideRepositorySearchPageObjects extends TestBase {
     RepositoryPage repositoryPage = new RepositoryPage();
+    MainPage mainPage;
 
-    @BeforeAll
-    static void setupAll() {
+    @BeforeEach
+    void setup() {
         Configuration.holdBrowserOpen = true;
+        mainPage = WelcomePage.openPage()
+                .clickButtonSignIn()
+                .setLogin("awerinzh@yandex.ru")
+                .setPassword("averin228337")
+                .clickSignInButton();
     }
+
 
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
 
-
-        WelcomePage.openPage()
-                .clickButtonSignIn()
-                .setLogin("awerinzh@yandex.ru")
-                .setPassword("averin228337")
-                .clickSignInButton()
+        mainPage
                 .clickTheInputField()
                 .enterInTheField("selenide")
                 .clickOnTheFirstLink()
                 .setShouldHave("selenide / selenide")
                 .hoverOverFirstContributor()
-                .verifyContributorName("Andrei Solntsev")
-                .clickOnTheIcon()
-                .clickSignOut()
-                .clickSignOutAgain();
+                .verifyContributorName("Andrei Solntsev");
 
 
     }
--- src/test/java/my_progect/TestBase.java
+++ src/test/java/my_progect/TestBase.java
@@ -1,7 +1,24 @@
 package my_progect;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import pages.RepositoryPage;
+import pages.components.MainPage;
+import pages.components.WelcomePage;
 
 public class TestBase {
     RepositoryPage repositoryPage = new RepositoryPage();
+
+
+    @AfterEach
+    void logoutAll() {
+        repositoryPage
+                .clickOnTheIcon()
+                .clickSignOut()
+                .clickSignOutAgain();
+    }
+
+
+
 }
