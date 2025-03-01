a2e553e4f80e97d2ec5809f72c4185d6494e4283
Caban <awerinzh@yandex.ru>
2025-03-01 12:30:51 +0300

    deleted

--- src/test/java/my_progect/TestBase.java
+++ src/test/java/my_progect/TestBase.java
@@ -1,11 +1,8 @@
 package my_progect;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import pages.RepositoryPage;
-import pages.components.MainPage;
-import pages.components.WelcomePage;
+
 
 public class TestBase {
     RepositoryPage repositoryPage = new RepositoryPage();
