19e9c5971be184426a1013a9b83207db759b881b
Caban <awerinzh@yandex.ru>
2025-03-10 08:14:41 +0300

    9.03

--- src/test/java/github/test/TestBase.java
+++ src/test/java/github/test/TestBase.java
@@ -1,11 +1,13 @@
 package github.test;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import github.pages.RegistrationPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -13,6 +15,14 @@ public class TestBase {
 
     @BeforeAll
     static void setUP() {
+        //Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadTimeout = 100000;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true));
